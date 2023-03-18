ce01f4b82d6d71072e4718f2143a0623d23a64a5
Juan Jacobo Ospina Garcia <jacoboospina@MacBook-Air-de-Juan.local>
2023-03-18 11:00:39 +0100

    added migrations and deleted not needed tables migrations

--- database/migrations/2023_03_18_094124_create_stock_table.php
+++ database/migrations/2023_03_18_094124_create_stock_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stock', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('shop_id');
+            $table->unsignedBigInteger('product_id');
+
+
+            $table->foreign('shop_id')->references('id')->on('shops');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stock');
     }
 };
